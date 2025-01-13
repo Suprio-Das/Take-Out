90f44652f52675f521d54913ca843b9df19f1d93
Suprio-Das <priyamd507@gmail.com>
2025-01-13 17:24:53 +0600

    Form Data fetched by a function

--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
 import { GoPersonFill } from "react-icons/go";
 const Signup = () => {
+    const [formData, setFormData] = useState({})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const data = {
+            name: form.username.value,
+            email: form.email.value,
+            password: form.password.value
+        }
+        console.log(data)
+    }
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="card card-compact bg-white w-full max-w-md px-5 py-3">
@@ -7,23 +20,23 @@ const Signup = () => {
                     <GoPersonFill />
                     <h2 className="text-xl font-semibold">Signup</h2>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="my-3">
-                        <label className="input input-bordered flex items-center gap-2">
+                        <label htmlFor="username" className="input input-bordered flex items-center gap-2">
                             Name
-                            <input type="text" className="grow" placeholder="Suprio Das" name="username" />
+                            <input type="text" className="grow" placeholder="Suprio Das" name="username" required />
                         </label>
                     </div>
                     <div className="my-3">
-                        <label className="input input-bordered flex items-center gap-2">
+                        <label htmlFor="email" className="input input-bordered flex items-center gap-2">
                             Email
-                            <input type="email" className="grow" placeholder="suprio.cse@gmail.com" name="email" />
+                            <input type="email" className="grow" placeholder="suprio.cse@gmail.com" name="email" required />
                         </label>
                     </div>
                     <div className="my-3">
-                        <label className="input input-bordered flex items-center gap-2">
+                        <label htmlFor="password" className="input input-bordered flex items-center gap-2">
                             Password
-                            <input type="password" className="grow" placeholder="Create a strong password" name="password" />
+                            <input type="password" className="grow" placeholder="Create a strong password" name="password" required />
                         </label>
                     </div>
                     <div className="my-3">
